388a670bbc8b097e11c89dbb49dc618a592e4e4b
amieka <a.sarkar.arun@gmail.com>
2021-12-02 09:49:53 -0500

    add virtualenv

--- day_1/main.py
+++ day_1/main.py
@@ -1,13 +1,13 @@
-
 import os
+
 
 def part_two():
     with open("input_2.txt") as ip:
         lines = ip.readlines()
         k = 3
-        start, end, increased = 0,3,0
+        start, end, increased = 0, 3, 0
         running_sum = sum([int(lines[x]) for x in range(3)])
-        while start >=0 and end <= len(lines):
+        while start >= 0 and end <= len(lines):
             inner_sum = 0
             for i in range(start, end, 1):
                 depth = int(lines[i])
@@ -15,10 +15,11 @@ def part_two():
             if inner_sum > running_sum:
                 increased += 1
             running_sum = inner_sum
-            
+
             start += 1
             end += 1
         print(increased)
+
 
 def solve():
     with open("input.txt") as ip:
@@ -33,6 +34,8 @@ def solve():
             else:
                 first = depth
         print(increased)
+
+
 if __name__ == "__main__":
     solve()
     part_two()
